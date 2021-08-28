71ef74ae5b1bbdd228f951abff87abaefa853293
marcomasulli <marco.masulli@gmail.com>
2021-08-28 15:26:21 +0700

    MM: updated schemas

--- app/api/models.py
+++ app/api/models.py
@@ -24,11 +24,11 @@ class Agent(Base):
     agent_phone = Column(String)
     agent_email = Column(String)
     agent_contact_person = Column(String)
-    is_active = Column(Boolean, default=True)
     agent_type = Column(String)
     agent_model = Column(String)
     agent_commission = Column(Float)
     agent_discount = Column(Float)
+    is_active = Column(Boolean, default=True)
 
 class Hotel(Base):
     __tablename__ = "hotels"
@@ -55,7 +55,96 @@ class Hotel(Base):
 
     rooms = relationship('Room', back_populates='hotel')
     hotel_info = relationship('HotelInfo', back_populates='hotel')
+    hotel_meeting_rooms = relationship('HotelServiceMeetingRoom', back_populates='hotel')
+    hotel_cancel_policies = relationship('HotelCancelPolicy', back_populates='hotel')
+    hotel_gds_info = relationship('HotelGDSInfo', back_populates='hotel')
+    hotel_amenity = relationship('HotelServiceAmenity', back_populates='hotel')
+    hotel_facility = relationship('HotelServiceFacility', back_populates='hotel')
 
+class HotelInfo(Base):
+    __tablename__ = "hotel_info"
+
+    id = Column(Integer, primary_key=True, index=True)
+    hotel_id = Column(Integer, ForeignKey("hotels.id"), index=True)
+    currency_code = Column(String, index=True)
+    currency_code_decimal_places = Column(Integer)
+    hotel_pms_system = Column(String)
+    location_category = Column(String)
+    segment_category = Column(String)
+    hotel_category = Column(String)
+    architectural_style = Column(String)
+    
+    hotel = relationship('Hotel', back_populates='hotel_info')
+
+class HotelServiceAmenity(Base):
+    __tablename__ = "hotel_amenities"
+
+    id = Column(Integer, primary_key=True, index=True)
+    hotel_id = Column(Integer, ForeignKey("hotels.id"), index=True)
+    description = Column(String)
+
+    hotel = relationship('Hotel', back_populates='hotel_amenity')
+
+class HotelServiceFacility(Base):
+    __tablename__ = "hotel_facilities"
+
+    id = Column(Integer, primary_key=True, index=True)
+    hotel_id = Column(Integer, ForeignKey("hotels.id"), index=True)
+    description = Column(String)
+
+    hotel = relationship('Hotel', back_populates='hotel_facility')
+
+class HotelServiceMeetingRoom(Base):
+    __tablename__ = "hotel_meeting_rooms"
+
+    id = Column(Integer, primary_key=True, index=True)
+    hotel_id = Column(Integer, ForeignKey("hotels.id"), index=True)
+    service_name = Column(String)
+    room_name = Column(String)
+    room_capacity = Column(Integer)
+    room_access = Column(String)
+    room_level = Column(String)
+    room_proximity = Column(String)
+    room_type = Column(String)
+    room_measure_unit = Column(String)
+    room_width = Column(Integer)
+    room_height = Column(Integer)
+    room_area = Column(Integer)
+    room_length = Column(Integer)
+
+    hotel = relationship('Hotel', back_populates='hotel_meeting_rooms')
+
+class HotelCancelPolicy(Base):
+    __tablename__ = "hotel_cancel_policies"
+
+    id = Column(Integer, primary_key=True, index=True)
+    hotel_id = Column(Integer, ForeignKey("hotels.id"), index=True)
+    # guaranteed_room_via_gds = Column(Boolean, default=True)
+    # guaranteed_room_via_crc = Column(Boolean, default=True)
+    # guaranteed_room_via_property = Column(Boolean, default=True)
+    date_start = Column(DateTime)
+    date_end = Column(DateTime)
+    weekdays = Column(String)
+    penalty_basis = Column(String, default='Hours')
+    penalty_deadline_type = Column(String, default='BeforeArrival')
+    penalty_deadline_value = Column(Integer, default=24)
+    penalty_amount_type = Column(String, default='Percent')
+    penalty_amount_value = Column(Integer, default=100)
+
+    hotel = relationship('Hotel', back_populates='hotel_cancel_policies')
+
+class HotelGDSInfo(Base):
+    __tablename__ = "hotel_gds_info"
+
+    id = Column(Integer, primary_key=True, index=True)
+    hotel_id = Column(Integer, ForeignKey("hotels.id"), index=True)
+    master_chain_code = Column(String)
+    gds_property_code = Column(String)
+    gds_name = Column(String)
+    chain_code = Column(String)
+    gds_status_value = Column(String, default='Closed')
+
+    hotel = relationship('Hotel', back_populates='hotel_gds_info')
 
 class Room(Base):
     __tablename__ = "rooms"
@@ -64,23 +153,56 @@ class Room(Base):
     hotel_id = Column(Integer, ForeignKey("hotels.id"))
     room_name = Column(String, index=True)
     room_quantity = Column(Integer)
-    room_description = Column (String)
+    room_description = Column(String)
     room_min_occupancy = Column(Integer)
     room_max_occupancy = Column(Integer)
     room_min_adults = Column(Integer)
     room_max_adults = Column(Integer)
     room_max_children = Column(Integer)
     room_max_infants = Column(Integer)
+    standard_bed_quantity = Column(Integer)
+    max_rollaways = Column(Integer)
+    room_size = Column(Float)
+    room_count = Column(Integer)
+    room_type_code = Column(String)
+    gds_room_type_code = Column(String)
+    floor = Column(Integer)
+    inventory_block_code = Column(String)
+    room_configuration = Column(String)
+    size_measurement = Column(String)
+    room_quantity = Column(Integer)
+    room_gender = Column(String)
+    max_cribs = Column(Integer)
+    non_smoking = Column(Boolean, default=True)
+    composite_ind = Column(Boolean)
+    share_room_ind = Column(Boolean)
+    room_category = Column(String)
+    room_location_code = Column(String)
+    room_view = Column(String)
+    bed_type = Column(String)
     min_rate = Column(Float)
     max_rate = Column(Float)
     fixed_rate = Column(Float)
-    rate_time_unit =  Column(String)
+    rate_time_unit = Column(String)
     is_active = Column(Boolean, default=True)
 
     hotel = relationship('Hotel', back_populates='rooms')
     rateplans = relationship('RatePlan', back_populates='room')
     availability = relationship('Availability', back_populates='room')
+    room_amenity = relationship('RoomAmenity', back_populates='room')
 
+class RoomAmenity(Base):
+    __tablename__ = "room_amenities"
+
+    id = Column(Integer, primary_key=True, index=True)
+    room_id = Column(Integer, ForeignKey("rooms.id"), index=True)
+    amenity_name = Column(String)
+    amenity_code = Column(String)
+    amenity_description = Column(String)
+    amenity_quantity = Column(Float)
+    included_in_rate = Column(Boolean, default=True)
+
+    room = relationship('Room', back_populates='room_amenity')
 class RatePlan(Base):
     __tablename__ = "rateplans"
 
@@ -106,47 +228,4 @@ class Availability(Base):
 
     room = relationship('Room', back_populates='availability')
 
-class HotelInfo(Base):
-    __tablename__ = "hotel_info"
-
-    id = Column(Integer, primary_key=True, index=True)
-    hotel_id = Column(Integer, ForeignKey("hotels.id"), index=True)
-    currency_code = Column(String, index=True)
-    currency_code_decimal_places = Column(Integer)
-    hotel_pms_system = Column(String)
-    location_category = Column(String)
-    segment_category = Column(String)
-    hotel_category = Column(String)
-    architectural_style = Column(String)
     
-    hotel = relationship('Hotels', back_populates='hotel_info')
-
-class HotelServiceAmenitY(Base):
-    __tablename__ = "hotel_AMENITY"
-
-    id = Column(Integer, primary_key=True, index=True)
-    hotel_id = Column(Integer, ForeignKey("hotels.id"), index=True)
-
-class HotelServiceFacility(Base):
-    __tablename__ = "hotel_facility"
-
-    id = Column(Integer, primary_key=True, index=True)
-    hotel_id = Column(Integer, ForeignKey("hotels.id"), index=True)
-
-class HotelServiceMeetingRoom(Base):
-    __tablename__ = "hotel_meeting_room"
-
-    id = Column(Integer, primary_key=True, index=True)
-    hotel_id = Column(Integer, ForeignKey("hotels.id"), index=True)
-    service_name = Column(String)
-    room_name = Column(String)
-    room_capacity = Column(Integer)
-    room_access = Column(String)
-    room_level = Column(String)
-    room_proximity = Column(String)
-    room_type = Column(String)
-    room_measure_unit = Column(String)
-    room_width = Column(Integer)
-    room_height = Column(Integer)
-    room_area = Column(Integer)
-    room_length = Column(Integer)
--- app/api/schemas.py
+++ app/api/schemas.py
@@ -63,6 +63,10 @@ class HotelBase(BaseModel):
     hotel_contact_person: Optional[str] = None
     hotel_contact_phone: Optional[str] = None
     hotel_contact_email: Optional[str] = None
+    hotel_code: Optional[str] = None
+    hotel_chain_code: Optional[str] = None
+    hotel_brand_code: Optional[str] = None
+    hotel_city_code: Optional[str] = None
 
 
 class HotelCreate(HotelBase):
@@ -76,17 +80,158 @@ class Hotel(HotelBase):
     class Config:
         orm_mode = True
 
+# HotelInfo
+
+class HotelInfoBase(BaseModel):
+    currency_code: Optional[str] = None
+    currency_code_decimal_places: Optional[int] = None
+    hotel_pms_system: Optional[str] = None
+    location_category: Optional[str] = None
+    segment_category: Optional[str] = None
+    hotel_category: Optional[str] = None
+    architectural_style: Optional[str] = None
+
+class HotelInfoCreate(HotelInfoBase):
+    pass
+
+class HotelInfo(HotelInfoBase):
+    id: int
+    hotel_id: int
+
+    class Config:
+        orm_mode = True
+
+
+# HotelServiceAmenity
+class HotelAmenityBase(BaseModel):
+    description: Optional[str]
+
+class HotelAmenityCreate(HotelAmenityBase):
+    pass
+
+class HotelAmenity(HotelAmenityBase):
+    id: int
+    hotel_id: int
+
+    class Config:
+        orm_mode = True
+
+# HotelServiceFacility
+class HotelFacilityBase(BaseModel):
+    description: Optional[str]
+    
+class HotelFacilityCreate(HotelFacilityBase):
+    pass
+
+class HotelFacility(HotelFacilityBase):
+    id: int
+    hotel_id: int
+
+    class Config:
+        orm_mode = True
+
+
+# Hotel Meeting Rooms
+class HotelMeetingRoomBase(BaseModel):
+    service_name: str
+    room_name: str
+    room_capacity: int
+    room_access: Optional[str] = None
+    room_level: Optional[str] = None
+    room_proximity: Optional[str] = None
+    room_type: Optional[str] = None
+    room_measure_unit: Optional[str] = None
+    room_width: Optional[int] = None
+    room_height: Optional[int] = None
+    room_area: Optional[int] = None
+    room_length: Optional[int] = None
+
+class HotelMeetingRoomCreate(HotelMeetingRoomBase):
+    pass
+
+class HotelMeetingRoom(HotelMeetingRoomBase):
+    id: int
+    hotel_id: int
+
+    class Config:
+        orm_mode = True
+
+# Hotel Cancellation Policies
+class HotelCancelPolicyBase(BaseModel):
+    date_start: datetime 
+    date_end: datetime 
+    weekdays: str 
+    penalty_basis: str 
+    penalty_deadline_type: str 
+    penalty_deadline_value: int 
+    penalty_amount_type: str 
+    penalty_amount_value: int 
+
+
+class HotelCancelPolicyCreate(HotelCancelPolicyBase):
+    pass
+
+class HotelCancelPolicy(HotelCancelPolicyBase):
+    id: int
+    hotel_id: int
+
+    class Config:
+        orm_mode = True
+
+# Hotel GDS Info
+
+class HotelGDSInfoBase(BaseModel):
+    master_chain_code: str
+    gds_property_code: str
+    gds_name: str
+    chain_code: str
+    gds_status_value: str
+
+class HotelGDSInfoCreate(HotelGDSInfoBase):
+    pass
+
+class HotelGDSInfo(HotelGDSInfoBase):
+    id: int
+    hotel_id: int
+
+    class Config:
+        orm_mode = True
+
 # Rooms
 
 class RoomBase(BaseModel):
     room_name: str
-    room_description: Optional[str]
-    room_min_occupancy: int
-    room_max_occupancy: int
-    room_min_adults: int
-    room_max_adults: int
-    room_max_children: int
-    room_max_infants: int
+    room_quantity: int = None
+    room_description: Optional[str] = None
+    room_min_occupancy: int = None
+    room_max_occupancy: int = None
+    room_min_adults: int = None
+    room_max_adults: int = None
+    room_max_children: int = None
+    room_max_infants: int = None
+    standard_bed_quantity: Optional[int] = None
+    max_rollaways: Optional[int] = None
+    room_size: Optional[float] = None
+    room_count: Optional[int] = None
+    room_type_code: Optional[str] = None
+    gds_room_type_code: Optional[str] = None
+    floor: Optional[int] = None
+    inventory_block_code: Optional[str] = None
+    room_configuration: Optional[str] = None
+    size_measurement: Optional[str] = None
+    room_gender: Optional[str] = None
+    max_cribs: Optional[int] = None
+    non_smoking: Optional[bool] = True
+    composite_ind: Optional[bool] = True
+    share_room_ind: Optional[bool] = True
+    room_category: Optional[str] = None
+    room_location_code: Optional[str] = None
+    room_view: Optional[str] = None
+    bed_type: Optional[str] = None
+    min_rate: Optional[float] = None
+    max_rate: Optional[float] = None
+    fixed_rate: Optional[float] = None
+    rate_time_unit: Optional[str] = None
 
 
 class RoomCreate(RoomBase):
@@ -100,6 +245,23 @@ class Room(RoomBase):
 
     class Config:
         orm_mode = True
+
+class RoomAmenityBase(BaseModel):
+    amenity_name: str
+    amenity_code: str
+    amenity_description: str
+    amenity_quantity: int = 0
+    included_in_rate: bool = True
+class RoomAmenityCreate(RoomAmenityBase):
+    pass
+
+class RoomAmenity(RoomAmenityBase):
+    id: int
+    hotel_id: int
+    
+    class Config:
+        orm_mode = True
+
 
 # Rates
 
